babab91fa828aed355fbc670b1632136408545a9
sumit <sumitkaranjkar741@gmail.com>
2024-09-08 17:51:10 +0530

    first

---
+++ js/worker.js
@@ -0,0 +1,13 @@
+"use strict";
+importScripts('game.js');
+importScripts('ai.js');
+
+onmessage = function(event) {
+    const game = Game.clone(event.data.game);
+    if (game.winner === null) {
+        const ai = new AI(event.data.numOfMCTSSimulations, event.data.uctConst, event.data.aiDevelopMode, true);
+        const move = ai.chooseNextMove(game);
+        postMessage(move);
+    }
+};
+
